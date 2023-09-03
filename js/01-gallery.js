523ff2933466e56dfa881c9744a079b1f567b37f
V-Yehor <es.valkoff@gmail.com>
2023-09-03 16:23:06 +0300

    01-gallery-js
    Є питання до завантаження картинки і до .min

---
+++ js/01-gallery.js
@@ -0,0 +1,36 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const list = document.querySelector('.gallery');
+const markUp = createMarcup(galleryItems);
+list.addEventListener('click', handleListClick);
+
+function createMarcup (arr) {
+    return arr.map(({ preview, original, description }) => {
+        return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}" target="_self" rel="noopener norefferer nofollow">
+        <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+        </a>
+        </li>`
+    }).join('');
+}
+
+list.innerHTML = markUp;
+
+function handleListClick(event) {
+    if (event.target === event.currentTarget) {
+        return;
+    }
+    const targetElement = event.target;
+    const originalUrl = targetElement.dataset.source;
+    
+    const instance = basicLightbox.create(
+        `<div class="modal">
+        <img src="${originalUrl}"/>
+        </div>`
+    )
+    // console.log(instance);
+    instance.show();
+}
